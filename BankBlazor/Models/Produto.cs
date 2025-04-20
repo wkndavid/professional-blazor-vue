e5349bc4130bc5e5f04b18e818ea1029b4afc083
wkndavid <webdeveloper@gmail.com>
2025-04-19 23:00:07 -0300

    [#issue] init - Validando Formulário com Blazor

--- BankBlazor/Models/Produto.cs
+++ BankBlazor/Models/Produto.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BankBlazor.Models
 
 {
     public class Produto
     {
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        [StringLength(50, ErrorMessage = "O campo Nome deve ter no máximo 50 caracteres.")]
+        [RegularExpression(@"^[a-zA-Z0-9\s]+$", ErrorMessage = "O campo Nome deve conter apenas letras, números e espaços.")]
         public string Nome { get; set;} = string.Empty;
+        [Range(0.1, 99999999, ErrorMessage ="Informe um valor adequado")]
         public int Quantidade { get; set;}
+        [Range(0.1, 99999999, ErrorMessage ="Informe um preço adequado")]
         public double Preco {get; set;}
         public double ValorEstoqueTotal()
         {
