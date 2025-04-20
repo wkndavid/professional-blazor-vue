821eb892d12f1aec698a7ef548e13a6b79e58f25
wkndavid <webdeveloper@gmail.com>
2025-04-20 02:02:03 -0300

    [ validation ] init - Validando inputs com DataAnotations e Usando ValidationMessage Validação com mensagem

--- BankBlazor/Models/Produto.cs
+++ BankBlazor/Models/Produto.cs
@@ -7,11 +7,11 @@ namespace BankBlazor.Models
         [Required(ErrorMessage = "O campo Nome é obrigatório.")]
         [StringLength(50, ErrorMessage = "O campo Nome deve ter no máximo 50 caracteres.")]
         [RegularExpression(@"^[a-zA-Z0-9\s]+$", ErrorMessage = "O campo Nome deve conter apenas letras, números e espaços.")]
-        public string Nome { get; set;} = " ";     
+        public string Nome { get; set; } = " ";     
         [Range(0.999, 9999999, ErrorMessage = "No campo Valor deve ser um valor adequado")]
-        public int Quantidade { get; set;}
+        public int Quantidade { get; set; }
         [Range(0.999, 9999999, ErrorMessage = "No campo Preço deve ser um preço válido")]
-        public double Preco {get; set;}
+        public double Preco { get; set; }
         public double ValorEstoqueTotal()
         {
             return Quantidade*Preco;
